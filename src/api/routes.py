3961a1450603151a377f80f757a8d57ba960fd62
Alba <allbaluccia@gmail.com>
2024-08-31 13:43:24 +0000

    routes api created

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,17 +6,62 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# @api.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
+
+        # Login
+
+@api.route("/signup", methods=["POST"])
+def create_user():
+    body = request.get_json()
+    user = User()
+    user.email = body.get("email")
+    user.password = body.get("password")
+    user.is_active = body.get("is_active")
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'msg':'User has been created successfully'}), 200
+
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # Query your database for email and password
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # Create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@api.route("/login", methods=["GET"])
+@jwt_required()
+def login():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    return jsonify({"id": user.id, "email": user.email}), 200
+    
