e03ce405de07cb61e92fcd1b887c1bb819ab1172
Alba <allbaluccia@gmail.com>
2024-09-01 17:01:52 +0000

    pages integradas

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,12 +5,12 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/login">
+					<span className="navbar-brand mb-0 h1">Login</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/signup">
+						<button className="btn btn-primary">Signup</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,17 +1,22 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Signup } from "./signup";
-import { Login } from "./login";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions} = useContext(Context)
 
-	return (
-		<div className="text-center mt-5">
-			<Signup />
-			<Login />
+  return (
+    <div className="container-fluid p-5">
+			<h1 className="mx-auto display-4 ms-5 d-flex justify-content-center text-white" >Developers</h1>
+			<div className="card jumbotron ms-5 mt-5 d-flex " style={{ width: 35 + "rem" }}>
+				<img src="https://thumbs.dreamstime.com/b/hola-mundo-letras-dibujadas-mano-del-vector-aislado-en-el-fondo-blanco-dise%C3%B1o-para-las-tarjetas-de-felicitaci%C3%B3n-d%C3%ADa-fiesta-144461394.jpg" />
+				<a className="btn btn-dark border-solid-dark btn-sm" href="https://www.linkedin.com/in/alba-rodr%C3%ADguez-594910198/" target="blank" role="button">LinkedIn</a>
+				
+
+			</div>
+			<span className="texto-j d-flex justify-content-center mt-5 mr-5 text-white"><strong></strong></span>
+
+		
 		</div>
-	);
+  );
 };
